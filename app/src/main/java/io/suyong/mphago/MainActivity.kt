60ae231b55e6e2c0f5f8ff61fea3218429f3ab3e
Su-Yong <simssy2205@gmail.com>
2020-11-28 15:49:21 +0900

    add Recommand Recyclerview

--- app/src/main/java/io/suyong/mphago/MainActivity.kt
+++ app/src/main/java/io/suyong/mphago/MainActivity.kt
@@ -2,6 +2,9 @@ package io.suyong.mphago
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.suyong.mphago.adapter.MainAdapter
+import io.suyong.mphago.adapter.RecommandType
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
+
+        val mainAdapter = MainAdapter(this)
+        mainRecyclerView.adapter = mainAdapter
+        mainRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        mainAdapter.list.add(RecommandType("test", "수락"))
+        mainAdapter.list.add(RecommandType("gd"))
     }
 }
